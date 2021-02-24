613baa1404d853b5a55bea43250c7ddf052c0da4
XzuluX <77434988+XzuluX@users.noreply.github.com>
2021-02-24 08:55:39 +0100

    Implemented Fw update over FoE (#56)
    * Implemented Fw update over FoE
    
    * Run processdata methods only in OP state

---
+++ src/EtherCAT.NET/Infrastructure/EthercatType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EtherCAT.NET.Infrastructure
+{
+    public enum SlaveState : ushort
+    {
+        // No valid state.
+        None = 0x00,
+        // Init state*
+        Init = 0x01,
+        // Pre-operational. 
+        PreOp = 0x02,
+        // Boot state
+        Boot = 0x03,
+        // Safe-operational.
+        SafeOp = 0x04,
+        // Operational 
+        Operational = 0x08,
+        // Error or ACK error 
+        Ack = 0x10,
+        Error = 0x10
+    }
+}
